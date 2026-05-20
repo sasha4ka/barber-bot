915ea62decc48f7097b2970f1b5e82e2b51d8a24
Aleksandr Basov <sasha4ka99991@gmail.com>
2026-05-20 14:23:06 +0300

    Rename: startup -> register_profile

--- src/book_bot/bot/handlers/__init__.py
+++ src/book_bot/bot/handlers/__init__.py
@@ -3,10 +3,10 @@ from aiogram import Router, types
 from book_bot.bot.general import server_error
 from book_bot.bot.handlers.add_appoinotment import router as add_appointment_router
 from book_bot.bot.handlers.main import router as main_menu_router
-from book_bot.bot.handlers.startup import router as startup_router
+from book_bot.bot.handlers.register import router as register_router
 
 router = Router()
-router.include_routers(startup_router, main_menu_router, add_appointment_router)
+router.include_routers(register_router, main_menu_router, add_appointment_router)
 
 
 fallback_router = Router()
