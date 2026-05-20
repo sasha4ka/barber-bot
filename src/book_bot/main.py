83abff766876348e7f922d02f7afe2c90deaa6fb
Aleksandr Basov <sasha4ka99991@gmail.com>
2026-05-20 19:51:00 +0300

    Add redis compatibility

--- src/book_bot/main.py
+++ src/book_bot/main.py
@@ -4,7 +4,11 @@ from typing import Any, Optional
 
 from aiogram import Bot, Dispatcher
 from aiogram.client.session.aiohttp import AiohttpSession
+from aiogram.fsm.storage.base import BaseStorage
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import RedisStorage
 from fastapi import FastAPI
+from redis.asyncio import Redis
 
 from book_bot.bot.handlers import router
 from book_bot.core.settings import settings
@@ -18,11 +22,19 @@ dp.include_router(router)
 async def lifespan(app: FastAPI):
     global bot
 
+    storage: Optional[BaseStorage] = None
+
+    if settings.REDIS_URL:
+        redis_client = Redis.from_url(url=settings.REDIS_URL)
+        storage = RedisStorage(redis=redis_client)
+    else:
+        storage = MemoryStorage()
+
     if settings.DEBUG and getattr(settings, "SOCKS5_PROXY", None):
         session = AiohttpSession(proxy=settings.SOCKS5_PROXY)
-        bot = Bot(token=settings.BOT_TOKEN, session=session)
+        bot = Bot(token=settings.BOT_TOKEN, storage=storage, session=session)
     else:
-        bot = Bot(token=settings.BOT_TOKEN)
+        bot = Bot(token=settings.BOT_TOKEN, storage=storage)
 
     if settings.DEBUG:
         print("Starting in long-pulling mode...")
