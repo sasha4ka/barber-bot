7c4af2de73b8064b1e9330ac30c30daf23adfd92
Aleksandr Basov <sasha4ka99991@gmail.com>
2026-05-21 19:02:17 +0300

    add tkq.py

---
+++ src/book_bot/tkq.py
@@ -0,0 +1,31 @@
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from taskiq_redis import RedisAsyncResultBackend, RedisStreamBroker
+
+from book_bot.core.settings import settings
+
+broker = RedisStreamBroker(url=settings.REDIS_URL).with_result_backend(
+    result_backend=RedisAsyncResultBackend(settings.BOT_TOKEN)
+)
+
+bot = Bot(token=settings.BOT_TOKEN)
+dp = Dispatcher()
+
+
+@broker.task
+async def send_message(user_id: int, text: str) -> None:
+    try:
+        await bot.send_message(user_id, text, parse_mode=ParseMode.HTML)
+    except Exception as er:
+        print(f"Error while sending message: {er}")
+
+
+@broker.on_event("startup")
+async def startup():
+    print("Notification server started")
+
+
+@broker.on_event("shutdown")
+async def shutdown():
+    await bot.session.close()
+    print("Notification server stopped")
