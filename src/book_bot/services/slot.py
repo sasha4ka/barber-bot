807d7807475692dc36d768791e591c0e9574a17d
Aleksandr Basov <sasha4ka99991@gmail.com>
2026-05-27 21:18:10 +0300

    FIX: unable to create slots for master without telegram profile

--- src/book_bot/services/slot.py
+++ src/book_bot/services/slot.py
@@ -4,7 +4,7 @@ from typing import Optional
 from sqlalchemy import select
 
 from book_bot.core.database import async_session
-from book_bot.models import Master, Slot
+from book_bot.models import Slot
 from book_bot.services.master import get_masters
 
 
@@ -16,19 +16,12 @@ async def generate_slots_for_date(
     slot_duration_minutes: int = 60,
     master_id: Optional[int] = None,
 ) -> list[Slot]:
-    if not master_id:
-        masters: list[Master] = await get_masters()
-        slots = []
-        for master in masters:
-            results = await generate_slots_for_date(
-                target_date=target_date,
-                work_end=work_end,
-                work_start=work_start,
-                slot_duration_minutes=slot_duration_minutes,
-                master_id=master.id,
-            )
-            slots.extend(results)  # type: ignore
-        return slots  # type: ignore
+    if master_id:
+        masters = [master_id]
+    else:
+        masters = [master.id for master in await get_masters()]
+
+    print(masters)
 
     async with async_session() as session:
         existing_slots_query = select(Slot).where(Slot.date == target_date)
@@ -36,21 +29,23 @@ async def generate_slots_for_date(
         if existing_slots_result.scalars().first():
             return []
 
-        current_datetime = datetime.datetime.combine(target_date, work_start)
+        step = datetime.timedelta(minutes=slot_duration_minutes)
         end_datetime = datetime.datetime.combine(target_date, work_end)
 
-        step = datetime.timedelta(minutes=slot_duration_minutes)
         new_slots: list[Slot] = []
 
-        while current_datetime + step <= end_datetime:
-            slot = Slot(
-                date=target_date,
-                time_start=current_datetime.time(),
-                is_booked=False,
-                master_id=master_id,
-            )
-            new_slots.append(slot)
-            current_datetime += step
+        for m_id in masters:
+            current_datetime = datetime.datetime.combine(target_date, work_start)
+
+            while current_datetime + step <= end_datetime:
+                slot = Slot(
+                    date=target_date,
+                    time_start=current_datetime.time(),
+                    is_booked=False,
+                    master_id=m_id,
+                )
+                new_slots.append(slot)
+                current_datetime += step
 
         if new_slots:
             session.add_all(new_slots)
