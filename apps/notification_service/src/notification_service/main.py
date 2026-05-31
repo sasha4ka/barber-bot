9e6636c04639fe1df8ade111a7b494d3cd9bc887
Aleksandr Basov <sasha4ka99991@gmail.com>
2026-05-31 11:41:07 +0300

    Fix notification service: not used core_shared library

--- apps/notification_service/src/notification_service/main.py
+++ apps/notification_service/src/notification_service/main.py
@@ -6,7 +6,6 @@ from aio_pika.abc import AbstractIncomingMessage
 from aiogram import Bot
 from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.enums import ParseMode
-
 from notification_service.settings import settings
 
 bot: Bot | None = None
