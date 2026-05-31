cb9f9988345c15ca0ed0f6babf97498a35a5cfc3
Aleksandr Basov <sasha4ka99991@gmail.com>
2026-05-31 11:59:25 +0300

    change envs: POSTGRES_URL instead of DB_* settings

--- apps/api/src/api/database.py
+++ apps/api/src/api/database.py
@@ -1,5 +1,4 @@
+from api.settings import settings
 from core_shared.database import Database
 
-from api.settings import settings
-
-db = Database(settings.get_database_url())
+db = Database(settings.POSTGRES_URL)
--- apps/bot/src/bot/core/settings.py
+++ apps/bot/src/bot/core/settings.py
@@ -12,16 +12,7 @@ class Settings(BaseSettings):
     ADMIN_TG: int
     DEBUG: bool = False
 
-    DB_NAME: str = "book_bot"
-    DB_USER: str = "book_bot_user"
-    DB_PASSWORD: str
-    DB_PORT: str = "5432"
-    DB_HOST: str = "localhost"
-
-    def get_database_url(self) -> str:
-        url = f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-        print(url)
-        return url
+    POSTGRES_URL: str
 
 
 settings = Settings()  # type: ignore
